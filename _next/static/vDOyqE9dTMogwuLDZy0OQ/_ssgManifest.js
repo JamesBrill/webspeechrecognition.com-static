ed838cfd9f8ef83e079d86ace81ca4fb17bd2650
James Brill <contact@jamesbrill.co.uk>
2022-05-14 19:31:12 +0100

    V6

---
+++ _next/static/vDOyqE9dTMogwuLDZy0OQ/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Freact-speech-recognition-demo","\u002Freact-speech-recognition","\u002Fwhat-is-web-speech-recognition","\u002Fpolyfills","\u002Freact-speech-recognition-api-docs"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
